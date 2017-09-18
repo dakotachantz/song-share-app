36603b9f7fa7ef28b20845fd508d3b99f9cc02c0
Dakota Nichols <dakotachantz@gmail.com>
2017-09-18 17:59:24 -0500

    made header stateless functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import reactScroll from "react-scroll";
 
 var Link = reactScroll.Link;
 
-export default class Header extends Component {
-
-    render() {
-        return (
-            <header id="page-top" className="masthead parallax">
-                <div className="header-content">
-                    <div className="header-content-inner">
-                        <h1 id="homeHeading">Your Favorite Song Sharing App is now Here!</h1>
-                        <hr />
-                        <p>Song Share can help you share songs your friends should listen to!</p>
-                        <Link className="btn btn-primary btn-xl"
-                            to="share"
-                            activeClass="activeScroll"
-                            spy={true}
-                            smooth={true}
-                            offset={0}
-                            duration={500}>Share a song</Link>
-                    </div>
+export default () => {
+    return (
+        <header id="page-top" className="masthead parallax">
+            <div className="header-content">
+                <div className="header-content-inner">
+                    <h1 id="homeHeading">Your Favorite Song Sharing App is now Here!</h1>
+                    <hr />
+                    <p>Song Share can help you share songs your friends should listen to!</p>
+                    <Link className="btn btn-primary btn-xl"
+                        to="share"
+                        activeClass="activeScroll"
+                        spy={true}
+                        smooth={true}
+                        offset={0}
+                        duration={500}>Share a song</Link>
                 </div>
-            </header>
-        )
-    }
+            </div>
+        </header>
+    )
 }
